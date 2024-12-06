c2297c6518305d09f961f2678374a2268ea7d456
Miguel Angel López Monroy <miguel.lopezm.dev@gmail.com>
2024-12-06 02:10:12 -0600

    chore: setup basic django project

---
+++ authors_api/settings/production.py
@@ -0,0 +1,7 @@
+from .base import *  # noqa
+from .base import env  # noqa
+
+ADMINS = [("Miguel Angel López Monroy", "miguel.lopezm.dev@mail.com")]
+
+# TODO Add doamin names of the production server
+CSRF_TRUSTED_ORIGINS = [""]
